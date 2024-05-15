5cc360f8e4fe89ab42e29eb6ca5b10cc64da1275
Oriana Martínez <martinez.oriana.e@gmail.com>
2024-05-14 23:16:11 -0400

    [UPDATE] BasicHeader | Index (actrices)

--- src/pages/actrices/Index.jsx
+++ src/pages/actrices/Index.jsx
@@ -9,7 +9,7 @@ import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
 /* Actrices Principales */
 const Index = () => (
     <PagesLayout title='Actrices Principales'>
-        <div className="mt-12 w-full max-w-4xl">
+        <div className="mt-4 w-full max-w-4xl sm:mt-12">
             <TabGroup>
                 <TabList className='flex items-center justify-center'>
                     {tabsActresses.map(({ id, name }) => (
@@ -18,7 +18,7 @@ const Index = () => (
                 </TabList>
 
                 <TabPanels className='border-2 border-violet-300 shadow-md sm:rounded-xl'>
-                    {tabsActresses.map(({ id, image, name, rol, }) => (
+                    {tabsActresses.map(({ id, birthday, description, image, name, rol, }) => (
                         <TabPanel className='p-6 flex flex-wrap items-center justify-center rounded-xl sm:flex-nowrap sm:justify-start' key={id}>
                             <img
                                 alt={name}
@@ -27,11 +27,21 @@ const Index = () => (
                             />
 
                             <div className="mx-0 mt-8 text-gray-800 sm:mx-8 sm:mt-0">
-                                <h2 className="text-blue text-2xl text-center font-basker font-bold sm:text-3xl">{ name }</h2>
+                                <h2 className="mb-4 text-blue text-2xl text-center font-basker font-bold sm:text-3xl">{ name }</h2>
 
-                                <div className="flex flex-col items-center text-sm sm:text-base">
-                                    <div className='mt-4'>
+                                <div className="pr-4 h-40 flex flex-col items-center text-sm overflow-auto sm:h-60 sm:text-base">
+                                    <div>
+                                        <span className="font-bold">Fecha de Nacimiento:</span> { birthday }
+                                    </div>
+                                    <div className='mt-1'>
                                         <span className="font-bold">Rol en la Película:</span> { rol }
+                                    </div>
+                                    <div className='mt-1.5'>
+                                        <div className=' text-center font-bold'>Descripción:</div>
+
+                                        {description.map((item, index) => (
+                                            <div className="mt-0.5 text-justify" key={index}>{ item }</div>
+                                        ))}
                                     </div>
                                 </div>
                             </div>
--- src/utils/helpers.jsx
+++ src/utils/helpers.jsx
@@ -23,18 +23,35 @@ export const navLinks = [
 export const tabsActresses = [
     {
         id: 1,
+        birthday: '11/07/1970',
+        description: [
+            'Su debut cinematográfico fue en la película policíaca Streetwise (1998). Su primer papel destacado fue en la película de comedia dramática del 2001 Baby Boy, donde interpretó a Yvette. Posteriormente, Henson ganó reconocimiento por interpretar a una prostituta en Hustle & Flow (2005) y a una madre soltera en The Curious Case of Benjamin Button (2008) de David Fincher. Esto último le valió una nominación al Premio de la Academia a la Mejor Actriz de Reparto.',
+            'En el 2016, interpretó a la matemática Katherine Johnson en Hidden Figures.  También ha actuado en  The Karate Kid (2010), Think Like a Man (2012), Acrimony (2018), What Men Want (2019), The Best of Enemies (2019) y The Color Purple (2023).',
+            'Ha recibido varios reconocimientos, incluyendo un Globo de Oro, así como nominaciones a un Premio de la Academia, un Premio Tony y cuatro Premios Primetime Emmy.',
+        ],
         image: images.actress1,
         name: 'Taraji P. Henson',
         rol: 'Katherine Johnson',
     },
     {
         id: 2,
+        birthday: '25/05/1970',
+        description: [
+            'Spencer hizo su debut cinematográfico en el drama de 1996 A Time to Kill. Después de una década de breves papeles en cine y televisión, su gran avance llegó en 2011 cuando interpretó a una sirvienta en los Estados Unidos de la década de 1960 en la película dramática The Help, que le valió el Premio de la Academia a la Mejor Actriz de Reparto.',
+            'Otros de los proyectos que ha protagonizado son The Divergent Series (2015-16), The Shack (2017), Gifted (2017), Instant Family (2018), Luce (2019), Ma (2019), Onward (2020) y Spirited (2022).',
+            'Ha recibido varios reconocimientos, entre ellos un Premio de la Academia, un Premio de la Academia Británica de Cine y un Globo de Oro, además de una nominación a un Premio Primetime Emmy.',
+        ],
         image: images.actress2,
         name: 'Octavia Spencer',
         rol: 'Dorothy Vaughan',
     },
     {
         id: 3,
+        birthday: '01/12/1985',
+        description: [
+            'Es una cantante, compositora, rapera y actriz estadounidense. Ha recibido diez nominaciones a los premios Grammy, y ha recibido un premio del Sindicato de Actores y un premio Emmy para niños y familias. Monáe también ha sido honrada con el Premio ASCAP Vanguard, así como con el Premio Rising Star (2015) y el Premio Trailblazer of the Year (2018) de Billboard Women in Music. ',
+            'En octubre de 2016, Monáe hizo su debut como actriz en la pantalla grande en la película aclamada por la crítica Moonlight, junto a Naomie Harris, André Holland y Mahershala Ali. También, ha protagonizado las películas Harriet (2019) y Glass Onion (2022), y la serie de televisión Homecoming (2020).',
+        ],
         image: images.actress3,
         name: 'Janelle Monáe',
         rol: 'Mary Jackson',
